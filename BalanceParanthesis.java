4b550947222482d8be1c755a5b3f47b89a9fc923
Samana Shrestha <samanashrestha02@gmail.com>
2022-05-05 08:09:10 +0545

    b

--- BalanceParanthesis.java
+++ BalanceParanthesis.java
@@ -17,5 +17,8 @@ public class BalanceParanthesis {
                     return false;
                 }
             }
+            if(stack.isEmpty()){
+                return false;
+            }
 }
         return true; }}
